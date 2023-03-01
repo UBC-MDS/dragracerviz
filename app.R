cdb9e6497be5c1e457552ce24d03f5f13feb03a0
Shaun Hutchinson <shaunhutch@shaw.ca>
2023-03-01 10:34:58 -0800

    Added season filter

--- app.R
+++ app.R
@@ -11,6 +11,8 @@ ui <- fluidPage(
     # sidebar (filters)
     sidebarPanel(
       'Filters',
+      selectInput(inputId = "season", label = "Season",
+                  choices = unique(drag_df$season)),
       # Other Categories filter
       checkboxGroupInput(inputId = "other_categories", label = "Other Categories",
                          choices = c("Miss Congeniality", "Winner", "Finalist", "First Eliminated"),
@@ -23,22 +25,23 @@ ui <- fluidPage(
       fluidRow()
     )
   )
-  
+
 )
 
 server <- function(input, output, session) {
   # reactive expression to filter data based on user selections
   filtered_data <- reactive({
     drag_filtered <- drag_df |>
+      dplyr::filter(season == input$season) |>
       dplyr::filter(if ("Miss Congeniality" %in% input$other_categories) missc == 1 else TRUE) |>
       dplyr::filter(if ("Winner" %in% input$other_categories) winner == 1 else TRUE) |>
       dplyr::filter(if ("Finalist" %in% input$other_categories) finalist == 1 else TRUE) |>
       dplyr::filter(if ("First Eliminated" %in% input$other_categories) first_eliminated == 1 else TRUE)
     drag_filtered
   })
-  
+
   # TBD: Create outcome tally table
-  
+
 }
 
 shinyApp(ui, server)
