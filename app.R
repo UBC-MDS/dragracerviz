4b985b282a1ac35fcf1bade9c862f7c572168f7a
Marian Agyby <marian9@student.ubc.ca>
2023-02-28 21:53:50 -0800

    Read in preprocessed dataset

--- app.R
+++ app.R
@@ -2,6 +2,9 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 
+# read in data
+data <- read_csv("data/drag.csv")
+
 ui <- fluidPage(
   titlePanel(title = 'Drag Race Visualizer'),
   sidebarLayout(
