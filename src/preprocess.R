a03862496927d88617ebd488bba5f12f065918cb
shaunhutch <112443522+shaunhutch@users.noreply.github.com>
2023-03-03 15:06:35 -0800

    Update preprocess.R
    Fixed typo

--- src/preprocess.R
+++ src/preprocess.R
@@ -76,7 +76,7 @@ main <- function(data, out_dir) {
                            city == "Toronto" ~ -79.3832,
                            city == "Van Nuys" ~ -118.4514,
                            TRUE ~ lng),
-           finalist_table = case_when(contestant %in% finalist_table$contestant ~ 1),
+           finalist = case_when(contestant %in% finalist_table$contestant ~ 1),
            winner = case_when(rank == 1 ~ 1),
            first_eliminated = case_when((episode == 1 & eliminated == 1) ~ 1,
                                         contestant == "Jaymes Mansfield" ~ 1,
