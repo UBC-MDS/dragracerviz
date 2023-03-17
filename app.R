1d559d68bb599196dc6f7c5833488480587a045a
dhruvinishar <dhruvi.bs19bds030@spjain.org>
2023-03-17 11:14:03 -0700

    added age slider updates to the graphs

--- app.R
+++ app.R
@@ -24,7 +24,7 @@ custom_theme <- bs_theme(
 drag_df <- read.csv("data/drag.csv")
 ui <- fluidPage(
   theme = custom_theme,
-  titlePanel(title = div(img(src ="logo.png", height = 100), 'Drag Race Visualizer')),
+  titlePanel(title = div(img(src ="logo.png", height = 100), 'Drag Race Visualizer', style = "font-size:60px;")),
   sidebarLayout(
     # sidebar (filters)
     sidebarPanel(
@@ -56,7 +56,8 @@ ui <- fluidPage(
       sliderInput(inputId = "age", label = "Age",
                   min = min(drag_df$age, na.rm = TRUE),
                   max = max(drag_df$age, na.rm = TRUE),
-                  value = c(min(drag_df$age, na.rm = TRUE), max(drag_df$age, na.rm = TRUE))),
+                  value = c(min(drag_df$age, na.rm = TRUE), max(drag_df$age, na.rm = TRUE)),
+                  step = 1),
       
       # Reset button
       actionButton(inputId = "reset", label = "Reset Filters")
@@ -67,18 +68,20 @@ ui <- fluidPage(
       fluidRow(
         column(7,
                h3("Hometown Map"),
+               'Queens Hometown and Where every Queen is from?',
                withSpinner(leafletOutput("hometown"),
                            color = "#FF1D8E")
                ),
         column(5,
                h3("Queen Performance"),
+               'Top 10 Queens over the Episodes',
                withSpinner(plotlyOutput("queen_challenge"),
                            color = "#FF1D8E")
         ),
       fluidRow(
         column(6,
           h3('Relative Rankings'),
-          'Ranking of the queen on their season and how many challenges they participated in on their season.',
+          'Ranking of the queen and how many challenges they participated in.',
           withSpinner(dataTableOutput('ranking'),
                       color = "#FF1D8E")
         ),
@@ -143,7 +146,15 @@ server <- function(input, output, session) {
     }
     
     # age filter always applies
-    drag_filtered <- drag_filtered |> dplyr::filter(age >= input$age[1] & age <= input$age[2])
+    drag_filtered <- drag_filtered |> 
+      dplyr::filter(age >= input$age[1] & age <= input$age[2])
+    
+    # update the age slider with the min and max values of the filtered data
+    updateSliderInput(session = session, 
+                      inputId = "age",
+                      min = min(drag_filtered$age, na.rm = TRUE),
+                      max = max(drag_filtered$age, na.rm = TRUE),
+                      value = c(min(drag_filtered$age, na.rm = TRUE), max(drag_filtered$age, na.rm = TRUE)))
     
     # name filter separate
     if (!is.null(input$queens)) {
@@ -157,7 +168,7 @@ server <- function(input, output, session) {
   observeEvent(input$reset, {
     updateSelectizeInput(session, "queens", selected = 'Jinkx Monsoon')
     updateSelectInput(session, "season", selected = NA)
-    updateSliderInput(session, "age", value = c(min(drag_df$age, na.rm = TRUE), max(drag_df$age, na.rm = TRUE)))
+    updateSliderInput(session, "age", min=21, max=52, value = c(21, 52))
     updateCheckboxGroupInput(session, "other_categories", selected = NA)
   })
 
